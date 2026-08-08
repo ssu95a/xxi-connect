53c3adc91582845d0a77001ba88dd94cfc20abce
sulimoff <ssu95a@gmail.com>
2026-08-08 23:36:42 +0300

    fixed/PGInitializer settings

--- src/main/java/ru/inversion/msrv/config/db/PGInitializer.java
+++ src/main/java/ru/inversion/msrv/config/db/PGInitializer.java
@@ -1,7 +1,6 @@
 package ru.inversion.msrv.config.db;
 
 import com.zaxxer.hikari.HikariConfig;
-import ru.inversion.utils.S;
 
 /** */
 public class PGInitializer implements IVendorDBSpecificInitializer {
@@ -9,18 +8,7 @@ public class PGInitializer implements IVendorDBSpecificInitializer {
     @Override
     public void initialize(HikariConfig hc)
     {
-//        hc.addDataSourceProperty( "useUnicode",           "true" );
-//        hc.addDataSourceProperty( "characterEncoding",    "utf8" );
         hc.addDataSourceProperty( "ApplicationName",      "xxi-connect" );
         hc.addDataSourceProperty( "escapeSyntaxCallMode", "callIfNoReturn" );
-
-        String poolName = hc.getPoolName();
-        if(!S.isNullOrEmpty(poolName) ) {
-            hc.setPoolName( poolName );
-        }
-
-//        hc.addDataSourceProperty( "preferQueryMode",      "extendedForPrepared" );
-//        hc.addDataSourceProperty( "defaultRowFetchSize",  "60"   );
-//        hc.addDataSourceProperty( "prepareThreshold",     "0"    );
     }
 }
